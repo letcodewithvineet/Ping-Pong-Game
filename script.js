0292b33f7afc17a907c10a262244d39314551ffd
letcodewithvineet <finalsolutioncode@gmail.com>
2023-12-03 19:23:32 +0530

    pingPongGame Code Reverted

--- script.js
+++ script.js
@@ -3,24 +3,6 @@ const bar1 = document.getElementById("bar-1");
 const bar2 = document.getElementById("bar-2");
 const ball = document.getElementById("ball");
 const movement = 20;
-const startButton = document.getElementById('start-button');
-
-let gameStarted = false;
-
-startButton.addEventListener('click', () => {
-    gameStarted = true;
-    startButton.style.display = 'none';
-});
-
-function gameLoop() {
-    if (gameStarted) {
-        // Update the positions of the ball and bars here
-        startGame();
-    }
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
 
 // Constants
 const thisBar1 = "Rod-1";
@@ -39,16 +21,14 @@ let highScore;
 let gameStart = false;
 
 // Set initial high score and player
-localStorage.setItem(storeName, "null");
+localStorage.setItem(storeScore, "null");
 localStorage.setItem(storeScore, "null");
 
 // Initialization function using IIFE
 (function () {
-    // Retrieve high score and player from localStorage
     highScore = localStorage.getItem(storeScore);
     whichBar = localStorage.getItem(storeName);
 
-    // Check if it's the first game
     if (whichBar === "null" || highScore === "null") {
         alert("Hello.. This is your first game");
         highScore = 0;
@@ -57,18 +37,15 @@ localStorage.setItem(storeScore, "null");
         alert(whichBar + " has the maximum score of " + highScore * 100);
     }
 
-    // Reset the game based on the player's bar
     gameReset(whichBar);
 })();
 
 // Function to reset the game
 function gameReset(barName) {
-    // Center the bars and ball horizontally
     bar1.style.left = ((window.innerWidth - bar1.offsetWidth) / 2) + "px";
     bar2.style.left = ((window.innerWidth - bar2.offsetWidth) / 2) + "px";
     ball.style.left = ((window.innerWidth - ball.offsetWidth) / 2) + "px";
 
-    // Set initial vertical position and direction for the ball
     if (barName === thisBar1) {
         ball.style.top = bar2.getBoundingClientRect().y - bar2.getBoundingClientRect().height + "px";
         moveY = -2;
@@ -77,7 +54,6 @@ function gameReset(barName) {
         moveY = 2;
     }
 
-    // Reset score and game status
     score = 0;
     gameStart = false;
 }
@@ -86,11 +62,17 @@ function gameReset(barName) {
 document.addEventListener('keydown', function (event) {
     // Move the bars using 'a' and 'd' keys
     if (event.keyCode == 68 || event.keyCode == 39) {
-        moveRight();
+        if (parseInt(bar1.style.left) < (window.innerWidth - bar1.offsetWidth - border)) {
+            bar1.style.left = parseInt(bar1.style.left) + movement + 'px';
+            bar2.style.left = bar1.style.left;
+        }
     }
 
     if (event.keyCode == 65 || event.keyCode == 37) {
-        moveLeft();
+        if (parseInt(bar1.style.left) > border) {
+            bar1.style.left = parseInt(bar1.style.left) - movement + 'px';
+            bar2.style.left = bar1.style.left;
+        }
     }
 
     // Start the game on 'Enter' key
@@ -102,57 +84,8 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-// Event listeners for touch events
-document.addEventListener('touchstart', handleTouchStart);
-document.addEventListener('touchmove', handleTouchMove);
-
-// Function to handle keydown events
-function handleKeyDown(event) {
-    switch (event.keyCode) {
-        case 68: // 'd' key or right arrow
-        case 39:
-            moveBar(movement);
-            break;
-        case 65: // 'a' key or left arrow
-        case 37:
-            moveBar(-movement);
-            break;
-        case 13: // 'Enter' key
-            if (!gameStart) {
-                gameStart = true;
-                startGame();
-            }
-            break;
-    }
-}
-
-// Function to handle touchstart events
-function handleTouchStart(event) {
-    event.preventDefault();
-    const touchX = event.touches[0].clientX;
-    moveBar(touchX > window.innerWidth / 2 ? movement : -movement);
-}
-
-// Function to handle touchmove events
-function handleTouchMove(event) {
-    event.preventDefault();
-    const touchX = event.touches[0].clientX;
-    moveBar(touchX > window.innerWidth / 2 ? movement : -movement);
-}
-
-// Function to move the bars
-function moveBar(offset) {
-    const newLeft = parseInt(bar1.style.left) + offset;
-    // Check if the new position is within the borders
-    if (newLeft >= border && newLeft <= (window.innerWidth - bar1.offsetWidth - border)) {
-        bar1.style.left = newLeft + 'px';
-        bar2.style.left = bar1.style.left;
-    }
-}
-
 // Function to start the game
 function startGame() {
-    // Retrieve initial positions and dimensions
     let ballRect = ball.getBoundingClientRect();
     let ballX = ballRect.x;
     let ballY = ballRect.y;
@@ -163,7 +96,6 @@ function startGame() {
     let bar1Width = bar2.offsetWidth;
     let bar2Width = bar2.offsetWidth;
 
-    // Move the ball at regular intervals
     ballMoving = setInterval(function () {
         let bar1X = bar1.getBoundingClientRect().x;
         let bar2X = bar2.getBoundingClientRect().x;
@@ -175,7 +107,7 @@ function startGame() {
         ball.style.left = ballX + "px";
         ball.style.top = ballY + "px";
 
-        // Check for ball collisions with bars and screen edges
+        // Check for ball collisions with bars
         if (((ballX + ballDia) > window.innerWidth) || (ballX < 0)) {
             moveX = -moveX;
         }
@@ -200,37 +132,15 @@ function startGame() {
     }, 10);
 }
 
-// Function to move the bars to the right
-function moveRight() {
-    if (parseInt(bar1.style.left) < (window.innerWidth - bar1.offsetWidth - border)) {
-        bar1.style.left = parseInt(bar1.style.left) + movement + 'px';
-        bar2.style.left = bar1.style.left;
-    }
-}
-
-// Function to move the bars to the left
-function moveLeft() {
-    if (parseInt(bar1.style.left) > border) {
-        bar1.style.left = parseInt(bar1.style.left) - movement + 'px';
-        bar2.style.left = bar1.style.left;
-    }
-}
-
 // Function to store data and reset the game
-function storeData(scoreObtained, winningBar) {
-    // Check if score is greater than highScore
-    if (scoreObtained > highScore) {
-        highScore = scoreObtained;
+function dataStoring(scoreObtained, winningBar) {
+    if (score > highScore) {
+        highScore = score;
         localStorage.setItem(storeName, winningBar);
         localStorage.setItem(storeScore, highScore);
     }
-
-    // Stop the ball movement and reset the game
     clearInterval(ballMoving);
     gameReset(winningBar);
 
-    // Display the result to the user
-    const maxScore = highScore * 100;
-    const scoreDisplay = scoreObtained * 100;
-    alert(`${winningBar} wins with a score of ${scoreDisplay}. Max Score is: ${maxScore}`);
+    alert(winningBar + " wins with a score of " + (scoreObtained * 100) + ". Max Score is: " + (highScore * 100));
 }
